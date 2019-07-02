c1dd0e2f062c0bd577293d4a9fd51413c9986138
Trung Ho <hmtrung901@gmail.com>
2019-07-02 19:47:51 +0700

    create test.sh

---
+++ WebDemo/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf results/*
+robot --variable BROWSER:firefox --variable GRID_URL:http://172.26.0.3:5555/wd/hub --variable CAPABILITIES:browserName:firefox,platform:LINUX --outputdir firefox_results login_tests &
+robot --variable BROWSER:chrome --variable GRID_URL:http://172.26.0.4:5555/wd/hub --variable CAPABILITIES:browserName:chrome,platform:LINUX --outputdir chrome_results login_tests &
+wait
+rebot --output output.xml firefox_results/output.xml chrome_results/output.xml
