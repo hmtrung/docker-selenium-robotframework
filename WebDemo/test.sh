cf7f212baa2e0a40a02aa561b34e994304c300a8
Trung Ho <hmtrung901@gmail.com>
2019-07-04 00:56:36 +0700

    Update test.sh

--- WebDemo/test.sh
+++ WebDemo/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf results/*
-robot --variable BROWSER:firefox --variable NODE_URL:http://${chrome_node}:5555/wd/hub --variable CAPABILITIES:browserName:firefox,platform:LINUX --outputdir firefox_results login_tests &
-robot --variable BROWSER:chrome --variable NODE_URL:http://${firefox_node}:5555/wd/hub --variable CAPABILITIES:browserName:chrome,platform:LINUX --outputdir chrome_results login_tests &
+robot --variable BROWSER:chrome --variable NODE_URL:http://${chrome_node}:5555/wd/hub --variable CAPABILITIES:browserName:chrome,platform:LINUX --outputdir firefox_results login_tests &
+robot --variable BROWSER:ff --variable NODE_URL:http://${firefox_node}:5555/wd/hub --variable CAPABILITIES:browserName:ff,platform:LINUX --outputdir chrome_results login_tests &
 wait
 rebot --output output.xml firefox_results/output.xml chrome_results/output.xml
