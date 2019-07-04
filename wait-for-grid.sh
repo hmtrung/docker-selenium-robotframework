8fce29b6199052b6b3ffd66be7056b3a8f04f462
Trung Ho <hmtrung901@gmail.com>
2019-07-04 13:03:54 +0700

    Update wait-for-grid.sh

--- wait-for-grid.sh
+++ wait-for-grid.sh
@@ -7,7 +7,7 @@ cmd="$@"
 unset http_proxy
 
 while ! curl -sSL "http://192.168.89.193:4444/wd/hub/status" 2>&1 \
-        | jq -r '.value.ready' 2>&1 | grep "true"; do
+        | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
     echo 'Waiting for the Grid'
     sleep 1
 done
